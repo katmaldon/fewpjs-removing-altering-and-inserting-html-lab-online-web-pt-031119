24551bb963f772d4aae3def5827b3adcfdf712c9
Kat Maldon <katharinamaldon@gmail.com>
2019-10-07 22:58:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-let ul = document.createElement('ul');
- 
-for (let i = 0; i < 3; i++) {
-  let li = document.createElement('li');
-  li.innerHTML = (i + 1).toString();
-  ul.appendChild(li);
-}
- 
-element.appendChild(ul);
+let header = document.getElementById("div#header");
+header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
